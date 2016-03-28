7a5005ab989b2e33da2be295d2f3efe1491a71d4
pankajmonga <pankaj.monga@live.com>
2016-03-28 16:09:04 +0530

    Add dummy data in user database

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,18 @@
+
+<?php
+
+class UserTableSeeder extends Seeder
+{
+
+public function run()
+{
+    DB::table('users')->delete();
+    User::create(array(
+        'name'     => 'admin',
+        'username' => 'admin',
+        'email'    => 'admin@admin.com',
+        'password' => Hash::make('1234'),
+    ));
+}
+
+}
