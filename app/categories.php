856c6c65457fb99dc0ea841520fc5861ee5802cd
pankajmonga <pankaj.monga@live.com>
2016-03-29 14:46:38 +0530

    add new files for category

--- app/categories.php
+++ app/categories.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Categories extends Model
 {
     //
-
+    protected $table = 'categories';
      protected $fillable = [
         'category', 'parent', 'status',
     ];
