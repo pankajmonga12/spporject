25325ac037c867ae717e0b6322a64e5074f61f4f
pankajmonga <pankaj.monga@live.com>
2016-03-29 14:30:49 +0530

    add new files for category

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Validator;
 use App\Http\Requests;
+use Resources\Views;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Auth;
 
 class CategoriesController extends Controller
 {
