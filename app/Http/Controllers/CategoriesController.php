555ad0fc2291f232b56118e81cffccf427b03378
pankajmonga <pankaj.monga@live.com>
2016-03-29 17:13:36 +0530

    add new files for category

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -9,6 +9,7 @@ use Resources\Views;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session;
 use App\Categories;
 class CategoriesController extends Controller
 {
