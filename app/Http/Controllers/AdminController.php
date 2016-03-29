a1cd6ffd01f4f3d0cc6d5c4f722dd800ee61adad
pankajmonga <pankaj.monga@live.com>
2016-03-29 19:10:50 +0530

    add new files for category

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -76,7 +76,10 @@ if ($validator->fails()) {
 {
     // show the form
     $categories = Categories::where('parent', '=', 0)->get();
-    $categoriesD = Categories::where('parent', '>', 0)->get();
+    $categoriesD = DB::table('categories')
+            ->join('categories as cat', 'cat.id', '=', 'categories.id')
+            ->select('categories.category as category','categories.id as id', 'categories.status as status','cat.category as parent')
+            ->get();
     $categoryData = array();
     $categoryDrop = array();
     $categoryDrop[0] ='Select Category';
