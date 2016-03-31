30fa8155dcf39fa2f0d426b7dc05e98c8ca67229
pankajmonga <pankaj.monga@live.com>
2016-03-31 18:12:18 +0530

    add elgibility option

--- app/Http/Controllers/AgelimitController.php
+++ app/Http/Controllers/AgelimitController.php
@@ -1,11 +1,17 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Validator;
 use App\Http\Requests;
-
+use Resources\Views;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session;
+use App\Agelimit;
 class AgelimitController extends Controller
 {
     //
