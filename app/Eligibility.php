b7bbbb30f53f1687c8ba2419bd7c06d94e918890
pankajmonga <pankaj.monga@live.com>
2016-03-31 15:51:10 +0530

    add ontroller and model files

--- app/Eligibility.php
+++ app/Eligibility.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class EligibilityModel extends Model
+class Eligibility extends Model
 {
     protected $table = 'eligibility';
      protected $fillable = [
