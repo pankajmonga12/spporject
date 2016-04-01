e656b6a69ee62be62646975328a6410a826d5683
pankajmonga <pankaj.monga@live.com>
2016-04-01 13:00:08 +0530

    add elgibility option

--- database/migrations/2016_04_01_071741_create_jobboard_table.php
+++ database/migrations/2016_04_01_071741_create_jobboard_table.php
@@ -14,6 +14,23 @@ class CreateJobboardTable extends Migration
     {
         Schema::create('jobboard', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('category');
+            $table->integer('subcategory');
+            $table->string('job_name');
+            $table->text('job_notification');
+            $table->string('descipline');
+            $table->dateTime('imp_date');
+            $table->integer('no_of_post');
+            $table->string('post_description');
+            $table->integer('eligibility');
+            $table->integer('qualification');
+            $table->string('exp_req');
+            $table->integer('age_limit');
+            $table->string('how_to_apply');
+            $table->integer('application_fees');
+            $table->string('website_link');
+            $table->string('logo');
+            $table->string('detailed_notification');
             $table->timestamps();
         });
     }
