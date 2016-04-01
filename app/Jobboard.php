17f40b5cfb6b8a516f0b7ba453235dacedc5e1b3
pankajmonga <pankaj.monga@live.com>
2016-04-01 20:41:41 +0530

    add elgibility option

--- app/Jobboard.php
+++ app/Jobboard.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Jobboard extends Model
 {
-    protected $table = 'joboard';
+    protected $table = 'jobboard';
      protected $fillable = [
           'category','subcategory','job_name','job_notification','descipline','imp_date','no_of_post','post_description','eligibility','qualification','exp_req','age_limit','how_to_apply','application_fees','website_link','logo',
     'detailed_notification' ];
