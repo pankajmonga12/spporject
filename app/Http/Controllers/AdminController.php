7b22fc43a5058af741a29b8eb4998db8f10535fb
pankajmonga <pankaj.monga@live.com>
2016-04-04 15:48:59 +0530

    add server pagination for job list grid

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -212,10 +212,10 @@ function showJobBoard()
      
      $users = Jobboard::paginate(1);
      $jobList = DB::table('jobboard')
-            ->join('categories as cat', 'cat.id', '=', 'Jobboard.category')
-            ->join('categories as scat', 'scat.id', '=', 'Jobboard.subcategory')
-            ->join('qualification as ql', 'ql.id', '=', 'Jobboard.qualification')
-            ->join('eligibility as el', 'el.id', '=', 'Jobboard.eligibility')
+            ->join('categories as cat', 'cat.id', '=', 'jobboard.category')
+            ->join('categories as scat', 'scat.id', '=', 'jobboard.subcategory')
+            ->join('qualification as ql', 'ql.id', '=', 'jobboard.qualification')
+            ->join('eligibility as el', 'el.id', '=', 'jobboard.eligibility')
             ->select('jobboard.id','cat.category as category', 'scat.category as subcategory', 'ql.title as qualification', 'el.title as eligibility','logo','jobboard.job_name','jobboard.job_notification','jobboard.imp_date','jobboard.no_of_post','jobboard.application_fees')->paginate(1);
     echo "<pre> Data  ".print_r($jobList , TRUE)."</pre>";
      // view('jobboardlist' , ['users' => $jobList]);
