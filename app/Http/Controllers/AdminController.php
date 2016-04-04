940ae1afc89c0b37793a91bb43701d8637a47497
pankajmonga <pankaj.monga@live.com>
2016-04-04 15:35:27 +0530

    add elgibility option

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -211,14 +211,15 @@ function showJobBoard()
    {
      
      $users = Jobboard::paginate(1);
+     DB::enableQueryLog();
      $jobList = DB::table('Jobboard')
             ->join('categories as cat', 'cat.id', '=', 'Jobboard.category')
             ->join('categories as scat', 'scat.id', '=', 'Jobboard.subcategory')
             ->join('qualification as ql', 'ql.id', '=', 'Jobboard.qualification')
             ->join('eligibility as el', 'el.id', '=', 'Jobboard.eligibility')
-            ->select('jobboard.id','cat.category as category', 'scat.category as subcategory', 'ql.title as category', 'el.title as category','logo','jobboard.job_name','jobboard.job_notification','jobboard.imp_date','jobboard.no_of_post','jobboard.application_fees');
-            
-     return view('jobboardlist' , ['users' => $users]);
+            ->select('jobboard.id','cat.category as category', 'scat.category as subcategory', 'ql.title as qualification', 'el.title as eligibility','logo','jobboard.job_name','jobboard.job_notification','jobboard.imp_date','jobboard.no_of_post','jobboard.application_fees');
+
+     return view('jobboardlist' , ['users' => $jobList]);
    	
    }
 }
--- resources/views/jobboardlist.blade.php
+++ resources/views/jobboardlist.blade.php
@@ -1,7 +1,68 @@
 <div class="container">
-    <?php echo "<pre> Job Data : ".print_r($users , TRUE)."</pre>"; ?>
+<table>
+	<tr>
+		<th>
+			S. No.
+		</th>
+		<th>
+			Job Name
+		</th>
+        <th>
+			Logo
+		</th>
+		<th>
+			Category
+		</th>
+		<th>
+			Sub - Category
+		</th>
+		<th>
+			Imp Date
+		</th>
+		<th>
+			No. Of Post
+		</th>
+		<th>
+			Aplication Fees
+		</th>
+		<th>
+			Action
+		</th>
+
+	</tr>
+</table>
+    
     @foreach ($users as $user)
-        {{ $user->job_name }}
+        <tr>
+        	<td>
+        		
+        	</td>
+        	<td>
+            {{ $user->job_name }}
+        	</td>
+        	<td>
+        <a href="http://128.199.192.88/public/logos/{{ $user->job_name }}" width="100" hight="100"></a>	
+        	</td>
+        	<td>
+        	{{ $user->job_name }}
+        	</td>
+        	<td>
+        	{{ $user->job_name }}
+        	</td>
+        	<td>
+        	{{ $user->job_name }}
+        	</td>
+        	<td>
+        	{{ $user->job_name }}
+        	</td>
+        	<td>
+        	{{ $user->job_name }}
+        	</td>
+        	<td>
+        	{{ $user->job_name }}
+        	</td>
+        </tr>
+        
     @endforeach
 </div>
 {!! $users->render() !!}
