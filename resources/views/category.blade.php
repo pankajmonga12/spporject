5642d6b92bacc0472f6f59345f86f8601f9e383c
pankajmonga <pankaj.monga@live.com>
2016-04-06 17:45:17 +0530

    add theme asset folder

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,68 +1,158 @@
-
-<!DOCTYPE html>
+<<!DOCTYPE html>
 <html>
-<head>
- <script   src="https://code.jquery.com/jquery-2.2.2.min.js"   integrity="sha256-36cp2Co+/62rEAAYHLmRCPIych47CvdM+uTBJwSzWjI="   crossorigin="anonymous"></script>	
-</head>
-<body>
+<?php echo ($header) ?> 
+<body class="page-header-fixed page-quick-sidebar-over-content page-sidebar-closed-hide-logo">
+<div class="page-header navbar navbar-fixed-top">
+	<!-- BEGIN HEADER INNER -->
+	<div class="page-header-inner">
+		<!-- BEGIN LOGO -->
+		<div class="page-logo">
+			<a href="index.html">
+			<img src="../../assets/admin/layout/img/logo.png" alt="logo" class="logo-default"/>
+			</a>
+			<div class="menu-toggler sidebar-toggler">
+			</div>
+		</div>
+		<!-- END LOGO -->
+		<!-- BEGIN HORIZANTAL MENU -->
+		<!-- DOC: Remove "hor-menu-light" class to have a horizontal menu with theme background instead of white background -->
+		<!-- DOC: This is desktop version of the horizontal menu. The mobile version is defined(duplicated) in the responsive menu below along with sidebar menu. So the horizontal menu has 2 seperate versions -->
+	
+		<!-- END HORIZANTAL MENU -->
+		<!-- BEGIN HEADER SEARCH BOX -->
+		<!-- DOC: Apply "search-form-expanded" right after the "search-form" class to have half expanded search box -->
+		<form class="search-form" action="extra_search.html" method="GET">
+			<div class="input-group">
+				<input type="text" class="form-control" placeholder="Search..." name="query">
+				<span class="input-group-btn">
+				<a href="javascript:;" class="btn submit"><i class="icon-magnifier"></i></a>
+				</span>
+			</div>
+		</form>
+		<!-- END HEADER SEARCH BOX -->
+		<!-- BEGIN RESPONSIVE MENU TOGGLER -->
+		<a href="javascript:;" class="menu-toggler responsive-toggler" data-toggle="collapse" data-target=".navbar-collapse">
+		</a>
+		<!-- END RESPONSIVE MENU TOGGLER -->
+		<!-- BEGIN TOP NAVIGATION MENU -->
+		<div class="top-menu">
+			<ul class="nav navbar-nav pull-right">
+				<!-- BEGIN NOTIFICATION DROPDOWN -->
+				<!-- DOC: Apply "dropdown-dark" class after below "dropdown-extended" to change the dropdown styte -->
+			
+				<!-- END INBOX DROPDOWN -->
+				<!-- BEGIN TODO DROPDOWN -->
+				<!-- DOC: Apply "dropdown-dark" class after below "dropdown-extended" to change the dropdown styte -->
+		
+				<!-- END TODO DROPDOWN -->
+				<!-- BEGIN USER LOGIN DROPDOWN -->
+				<!-- DOC: Apply "dropdown-dark" class after below "dropdown-extended" to change the dropdown styte -->
+				<li class="dropdown dropdown-user">
+					<a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+					<img alt="" class="img-circle" src="../../assets/admin/layout/img/avatar3_small.jpg"/>
+					<span class="username username-hide-on-mobile">
+					Admin </span>
+					
+					</a>
+					<ul class="dropdown-menu dropdown-menu-default">
+						<li>
+							<a href="extra_profile.html">
+							<i class="icon-user"></i> My Profile </a>
+						</li>
+						<li>
+							<a href="login.html">
+							<i class="icon-key"></i> Log Out </a>
+						</li>
+					</ul>
+				</li>
+				<!-- END USER LOGIN DROPDOWN -->
+				<!-- BEGIN QUICK SIDEBAR TOGGLER -->
+				<!-- DOC: Apply "dropdown-dark" class after below "dropdown-extended" to change the dropdown styte -->
+				<li class="dropdown dropdown-quick-sidebar-toggler">
+					<a href="javascript:;" class="dropdown-toggle">
+					<i class="icon-logout"></i>
+					</a>
+				</li>
+				<!-- END QUICK SIDEBAR TOGGLER -->
+			</ul>
+		</div>
+		<!-- END TOP NAVIGATION MENU -->
+	</div>
+	<!-- END HEADER INNER -->
+</div>
+<!-- END HEADER -->
+<div class="clearfix">
+</div>
+<!-- BEGIN CONTAINER -->
+<div class="page-container">
+	<!-- BEGIN SIDEBAR -->
+	<div class="page-sidebar-wrapper">
+		<!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
+		<!-- DOC: Change data-auto-speed="200" to adjust the sub menu slide up/down speed -->
+		<div class="page-sidebar navbar-collapse collapse">
+			<!-- BEGIN SIDEBAR MENU1 -->
+			<ul class="page-sidebar-menu hidden-sm hidden-xs" data-auto-scroll="true" data-slide-speed="200">
+				<!-- DOC: To remove the search box from the sidebar you just need to completely remove the below "sidebar-search-wrapper" LI element -->
+				<!-- DOC: This is mobile version of the horizontal menu. The desktop version is defined(duplicated) in the header above -->
+			
+			
+				
+				<?php echo $submenu; ?>
+				
+			</ul>
+			<!-- END SIDEBAR MENU1 -->
+			<!-- END RESPONSIVE MENU FOR HORIZONTAL & SIDEBAR MENU -->
+		</div>
+	</div>
+	<!-- END SIDEBAR -->
+	<!-- BEGIN CONTENT -->
+	<div class="page-content-wrapper">
+		<div class="page-content">
+			<!-- BEGIN SAMPLE PORTLET CONFIGURATION MODAL FORM-->
+			<!-- /.modal -->
+			<!-- END SAMPLE PORTLET CONFIGURATION MODAL FORM-->
+			<!-- BEGIN STYLE CUSTOMIZER -->
 
-@if(Session::has('message'))
-<p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('message') }}</p>
-@endif
-	<tr>
-	  <td>
-		<a href="/index.php/category">Add Category</a>
-	  </td>
-	  <td>
-		<a href="/index.php/subcategory">Add SubCategory</a>
-	  </td>
+			<!-- END STYLE CUSTOMIZER -->
+			<!-- BEGIN PAGE HEADER-->
+			<h3 class="page-title">
+			HOME
+			</h3>
+			<div class="page-bar">
+				<ul class="page-breadcrumb">
+					<li>
+						<i class="fa fa-home"></i>
+						<a href="index.html">Home</a>
+						<i class="fa fa-angle-right"></i>
+					</li>
+				</ul>
+			</div>
+			<!-- END PAGE HEADER-->
+			<!-- BEGIN PAGE CONTENT-->
 
-	  <td>
-		<a href="/index.php/subcategory">Add SubCategory</a>
-	  </td>
-	  
+			</div>
+			<div class="row">
+				
+				{!! Form::open(array('url' => 'savecategory' , 'method' => 'post')) !!}
+				<h1>Add New Category</h1>
 
-	  <td>
-		<a href="/index.php/addeligibility">Add Eligibilities</a>
-	  </td>
+				<!-- if there are login errors, show them here -->
+				<p>
+				    {{ $errors->first('category') }}
+				</p>
 
-	  <td>
-		<a href="/index.php/addagelimit">Add age limit</a>
-	  </td>
-
-	  <td>
-		<a href="/index.php/addqualification">Add Qualification</a>
-	  </td>
-
-	  <td>
-		<a href="/index.php/addjob">Add New Job</a>
-	  </td>
-
-	  <td>
-		<a href="/index.php/jobboardlist">Job Listing</a>
-	  </td>
-	</tr>
-</table>
-
-{!! Form::open(array('url' => 'savecategory' , 'method' => 'post')) !!}
-<h1>Add New Category</h1>
-
-<!-- if there are login errors, show them here -->
-<p>
-    {{ $errors->first('category') }}
-</p>
-
-<p>
-    {!!  Form::label('category', 'Category Name')!!}
-    {!!  Form::text('category', Input::old('category'), array('placeholder' => 'Category Name')) !!}
-</p>
+				<p>
+				    {!!  Form::label('category', 'Category Name')!!}
+				    {!!  Form::text('category', Input::old('category'), array('placeholder' => 'Category Name')) !!}
+				</p>
 
 
-<p>{!! Form::submit('Submit!') !!}</p>
-{!! Form::close() !!}
-
-<?php //echo "<pre> Data : ".print_r($categories , TRUE)."</pre>";  ?>
-<meta name="csrf-token" content="{{ csrf_token() }}" />
+				<p>{!! Form::submit('Submit!') !!}</p>
+				{!! Form::close() !!}
+			</div>
+			<!-- END PAGE CONTENT-->
+			<div class="row">
+				<meta name="csrf-token" content="{{ csrf_token() }}" />
 <table border="1">
 	<tr>
 		<td>
@@ -108,8 +198,32 @@
 	  </tr>
 	<?php } ?>
   </table>
- </body>
+			</div>
+	</div>
+	<!-- END CONTENT -->
+	<!-- BEGIN QUICK SIDEBAR -->
+	
+		</div>
+	</div>
+	<!-- END QUICK SIDEBAR -->
+</div>
+</body>
+<footer>
+	<?php echo ($footer) ?> 
+</footer>
+<script>
+    jQuery(document).ready(function() {    
+       Metronic.init(); // init metronic core components
+Layout.init(); // init current layout
+QuickSidebar.init(); // init quick sidebar
+Demo.init(); // init demo features
+    });
+  </script>
+
 </html>
+
+
+ 
 <script type="text/javascript">
 	function Deletecat(id) {
    
