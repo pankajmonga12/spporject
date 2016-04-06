8ab78f4978174ebde311e9362a9dd4f646fbb9ff
pankajmonga <pankaj.monga@live.com>
2016-04-06 14:51:00 +0530

    add theme asset folder

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -423,31 +423,9 @@ License: You must have a valid license purchased only from themeforest(the above
 	</form>
 	<!-- END REGISTRATION FORM -->
 </div>
-<div class="copyright">
-	 2014 © Metronic. Admin Dashboard Template.
-</div>
-<!-- END LOGIN -->
-<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
-<!-- BEGIN CORE PLUGINS -->
-<!--[if lt IE 9]>
-<script src="../../assets/global/plugins/respond.min.js"></script>
-<script src="../../assets/global/plugins/excanvas.min.js"></script> 
-<![endif]-->
-<script src="{{ url('/assets/global/plugins/jquery.min.js') }}" type="text/javascript"></script>
-<script src="{{ url('/assets/global/plugins/jquery-migrate.min.js') }}" type="text/javascript"></script>
-<script src="{{ url('/assets/global/plugins/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
-<script src="{{ url('/assets/global/plugins/jquery.blockui.min.js') }}" type="text/javascript"></script>
-<script src="{{ url('/assets/global/plugins/jquery.cokie.min.js') }}" type="text/javascript"></script>
-<script src="{{ url('/assets/global/plugins/uniform/jquery.uniform.min.js') }}" type="text/javascript"></script>
-<!-- END CORE PLUGINS -->
-<!-- BEGIN PAGE LEVEL PLUGINS -->
-<script src="{{ url('/assets/global/plugins/jquery-validation/js/jquery.validate.min.js') }}" type="text/javascript"></script>
-<!-- END PAGE LEVEL PLUGINS -->
-<!-- BEGIN PAGE LEVEL SCRIPTS -->
-<script src="{{ url('/assets/global/scripts/metronic.js') }}" type="text/javascript"></script>
-<script src="{{ url('/assets/admin/layout/scripts/layout.js') }}" type="text/javascript"></script>
-<script src="{{ url('/assets/admin/layout/scripts/demo.js') }}" type="text/javascript"></script>
-<script src="{{ url('/assets/admin/pages/scripts/login.js') }}" type="text/javascript"></script>
+<?php
+   echo View::make('partials.footer'); 
+?>
 <!-- END PAGE LEVEL SCRIPTS -->
 <script>
 jQuery(document).ready(function() {     
---
+++ resources/views/partial/footer.php
@@ -0,0 +1,25 @@
+<div class="copyright">
+	 2014 © Metronic. Admin Dashboard Template.
+</div>
+<!-- END LOGIN -->
+<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
+<!-- BEGIN CORE PLUGINS -->
+<!--[if lt IE 9]>
+<script src="../../assets/global/plugins/respond.min.js"></script>
+<script src="../../assets/global/plugins/excanvas.min.js"></script> 
+<![endif]-->
+<script src="http://128.199.192.88/assets/global/plugins/jquery.min.js" type="text/javascript"></script>
+<script src="http://128.199.192.88/assets/global/plugins/jquery-migrate.min.js" type="text/javascript"></script>
+<script src="http://128.199.192.88/assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="http://128.199.192.88/assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+<script src="http://128.199.192.88/assets/global/plugins/jquery.cokie.min.js" type="text/javascript"></script>
+<script src="http://128.199.192.88/assets/global/plugins/uniform/jquery.uniform.min.js'" type="text/javascript"></script>
+<!-- END CORE PLUGINS -->
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+<script src="http://128.199.192.88/assets/global/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
+<!-- END PAGE LEVEL PLUGINS -->
+<!-- BEGIN PAGE LEVEL SCRIPTS -->
+<script src="http://128.199.192.88/assets/global/scripts/metronic.js" type="text/javascript"></script>
+<script src="http://128.199.192.88/assets/admin/layout/scripts/layout.js" type="text/javascript"></script>
+<script src="http://128.199.192.88/assets/admin/layout/scripts/demo.js" type="text/javascript"></script>
+<script src="http://128.199.192.88/assets/admin/pages/scripts/login.js" type="text/javascript"></script>
