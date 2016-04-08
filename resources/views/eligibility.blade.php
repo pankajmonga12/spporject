31ad71af9701d2bf22a602388543742083740d60
pankajmonga <pankaj.monga@live.com>
2016-04-08 16:45:13 +0530

    change layout for job form

--- resources/views/eligibility.blade.php
+++ resources/views/eligibility.blade.php
@@ -135,7 +135,7 @@
 								<div class="form-actions">
 									<div class="row">
 										<div class="col-md-offset-3 col-md-9">
-											<button type="submit" class="btn green">Save Cateogry</button>
+											<button type="submit" class="btn green">Save Eligibility</button>
 											<button type="button" class="btn default" data-dismiss="modal">Close</button>
 										</div>
 									</div>
