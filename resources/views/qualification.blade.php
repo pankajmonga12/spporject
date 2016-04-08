72d96ea7436d38706551eeeb3417d088e7d8f6ea
pankajmonga <pankaj.monga@live.com>
2016-04-08 17:59:10 +0530

    change layout for job form

--- resources/views/qualification.blade.php
+++ resources/views/qualification.blade.php
@@ -107,7 +107,136 @@
 	<!-- END SIDEBAR -->
 	<!-- BEGIN CONTENT -->
 	<div class="page-content-wrapper">
-		<div class="page-content">
+		<div class="page-content">	
+		<div class="modal fade" id="change-qualification" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+							<h4 class="modal-title"></h4>
+						</div>
+						<div class="modal-body">
+							<div class="portlet light bordered">
+					<?php //echo $this->session->flashdata('error_msg'); ?>
+						<div class="portlet-title">
+							<div class="caption font-red-sunglo">
+								<i class="icon-settings font-red-sunglo"></i>
+								<span class="caption-subject bold uppercase"> CHANGE QUALIFICATON</span>
+							</div>
+					
+						</div>
+						<div class="portlet-body form">
+							<!-- BEGIN FORM-->
+							<form action="/changeeligibility" id="changepasswordform" class="form-horizontal"  method="POST">
+								<div class="form-body">
+									<div class="alert alert-danger display-hide">
+										<button class="close" data-close="alert"></button>
+										You have some form errors. Please check below.
+									</div>
+									<div class="alert alert-success display-hide">
+										<button class="close" data-close="alert"></button>
+										Your form validation is successful!
+									</div>
+									
+									<div class="form-group">
+										<label class="control-label col-md-3">New Qualification <span class="required">
+										* </span>
+										</label>
+										<div class="col-md-7">
+											<div class="input-icon right">
+												<i class="fa"></i>
+												<input type="hidden" class="form-control" id="qid" name="id"/>
+												<input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+												<input type="text" class="form-control" id="qualification" name="title"/>
+											</div>
+										</div>
+									</div>
+									
+								</div>
+								<div class="form-actions">
+									<div class="row">
+										<div class="col-md-offset-3 col-md-9">
+											<button type="submit" class="btn green">Save Qualification</button>
+											<button type="button" class="btn default" data-dismiss="modal">Close</button>
+										</div>
+									</div>
+								</div>
+							</form>
+							<!-- END FORM-->
+						</div>
+					</div>
+						</div>
+					</div>
+					<!-- /.modal-content -->
+				</div>
+				<!-- /.modal-dialog -->
+			</div>
+
+
+			<div class="modal fade" id="change-eligibility" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+							<h4 class="modal-title"></h4>
+						</div>
+						<div class="modal-body">
+							<div class="portlet light bordered">
+					<?php //echo $this->session->flashdata('error_msg'); ?>
+						<div class="portlet-title">
+							<div class="caption font-red-sunglo">
+								<i class="icon-settings font-red-sunglo"></i>
+								<span class="caption-subject bold uppercase"> CHANGE ELIGIBILITY</span>
+							</div>
+					
+						</div>
+						<div class="portlet-body form">
+							<!-- BEGIN FORM-->
+							<form action="/changeeligibility" id="changepasswordform" class="form-horizontal"  method="POST">
+								<div class="form-body">
+									<div class="alert alert-danger display-hide">
+										<button class="close" data-close="alert"></button>
+										You have some form errors. Please check below.
+									</div>
+									<div class="alert alert-success display-hide">
+										<button class="close" data-close="alert"></button>
+										Your form validation is successful!
+									</div>
+									
+									<div class="form-group">
+										<label class="control-label col-md-3">New Eligibility <span class="required">
+										* </span>
+										</label>
+										<div class="col-md-7">
+											<div class="input-icon right">
+												<i class="fa"></i>
+												<input type="hidden" class="form-control" id="eid" name="id"/>
+												<input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+												<input type="text" class="form-control" id="eligibility" name="title"/>
+											</div>
+										</div>
+									</div>
+									
+								</div>
+								<div class="form-actions">
+									<div class="row">
+										<div class="col-md-offset-3 col-md-9">
+											<button type="submit" class="btn green">Save Eligibility</button>
+											<button type="button" class="btn default" data-dismiss="modal">Close</button>
+										</div>
+									</div>
+								</div>
+							</form>
+							<!-- END FORM-->
+						</div>
+					</div>
+						</div>
+					</div>
+					<!-- /.modal-content -->
+				</div>
+				<!-- /.modal-dialog -->
+			</div>
+
 			<!-- BEGIN SAMPLE PORTLET CONFIGURATION MODAL FORM-->
 			<!-- /.modal -->
 			<!-- END SAMPLE PORTLET CONFIGURATION MODAL FORM-->
@@ -205,7 +334,7 @@
 		<td>
 			Status
 		</td>
-		<td>
+		<td colspan="2">
 			Action
 		</td>
 	</tr> 
@@ -227,7 +356,10 @@
 	  	</td>
 	  	<td>
 	  	 <input type="button" onclick="Deletecat(<?php echo $qualification[$i]['id']; ?>)" value="Delete"> </input>
-	  	 	  	</td>
+	  	</td>
+	  	<td>
+	  	 	<button onclick="changeQualification('<?php echo $eligibility[$i]['title']; ?>' , <?php echo $eligibility[$i]['id']; ?>)">Edit</button>
+	  	 </td>
 	  </tr>
 	<?php } ?>
   </table>
@@ -290,4 +422,14 @@ Demo.init(); // init demo features
     }
     
 }
+</script>
+
+
+<script type="text/javascript">
+	function changeQualification(qValue , id) {
+
+    document.getElementById('qualification').value = qValue;
+    document.getElementById('qid').value = id;
+		$('#change-qualification').modal('show');
+	}
 </script>
