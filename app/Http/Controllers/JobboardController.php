e3323f66ac3146f51c88661df92d6e88d5df1e4b
pankajmonga <pankaj.monga@live.com>
2016-04-15 13:50:30 +0530

    null value issue resolved

--- app/Http/Controllers/JobboardController.php
+++ app/Http/Controllers/JobboardController.php
@@ -201,7 +201,7 @@ class JobboardController extends Controller
             ->leftJoin('eligibility as el', 'el.id', '=', 'posttype.eligibility')
             ->where('post_id', '=', $id)
             ->select('posttype.id','posttype','ql.title as qualification', 'el.title as eligibility','no_of_post')->get();
-   echo "<pre> Data  ".print_r($posttype , TRUE)."</pre>";
+  // echo "<pre> Data  ".print_r($posttype , TRUE)."</pre>";
 //$posttype = Posttype::where('post_id', '=',  $id)->get();
 
        $posttypeData = array();
