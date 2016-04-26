fac35877e4f3827d3501fb509d6adaccbebcc293
pankajmonga <pankaj.monga@live.com>
2016-04-26 14:27:21 +0530

    add home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -55,6 +55,25 @@ class HomeController extends Controller
      function Jobsearch( Request $request )
     {
       
+      $eligibilities = Eligibility::where('status', '=', 1)->get();
+      $eligibilityData = array();
+      $eligibilityData[0] ='Select Eligibility';
+    foreach ($eligibilities as $eligibility) {
+
+    	$eligibilityData[$eligibility->id] = $eligibility->title;
+    }
+
+
+     $qualification = Qualification::where('status', '=', 1)->get();
+     $qualificationData = array();
+     $qualificationData[0] ='Select Qualification';
+    foreach ($qualification as $qualificationm) {
+    	$qualificationData[$qualificationm->id] = $qualificationm->title;
+    }
+     
+     $data['eligibility'] = $eligibilityData;
+	 $data['qualification'] = $qualificationData;
+	 
       $job_name = $request->input('job_name');
       $eligibility = $request->input('eligibility');
       $qualification = $request->input('qualification');
