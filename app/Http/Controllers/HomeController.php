86cddfc7572b49b261c8adc00c7b022d8daf9768
pankajmonga <pankaj.monga@live.com>
2016-04-27 12:14:20 +0530

    add home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -149,7 +149,7 @@ class HomeController extends Controller
             ->join('eligibility as el', 'el.id', '=', 'jobboard.eligibility')
             ->where('jobboard.id', '=', $id )
             ->select('jobboard.id','cat.category as category', 'scat.category as subcategory', 'ql.title as qualification', 'el.title as eligibility','logo','jobboard.job_name','jobboard.job_notification','jobboard.imp_date','jobboard.no_of_post','jobboard.application_fees')->get();
-           DB::getQueryLog();
+           dd(DB::getQueryLog());
             echo "<pre> Data : ".print_r($jobList , TRUE)."</pre>";
     }
 }
