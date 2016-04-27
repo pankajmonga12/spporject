99efe8d98a94ccb5575fe633c6a5f646dab96443
pankajmonga <pankaj.monga@live.com>
2016-04-27 12:16:25 +0530

    add home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -143,10 +143,10 @@ class HomeController extends Controller
     	DB::enableQueryLog();
     	echo "Data : ".$id;
     	$jobList = DB::table('jobboard')
-            ->join('categories as cat', 'cat.id', '=', 'jobboard.category')
-            ->join('categories as scat', 'scat.id', '=', 'jobboard.subcategory')
-            ->join('qualification as ql', 'ql.id', '=', 'jobboard.qualification')
-            ->join('eligibility as el', 'el.id', '=', 'jobboard.eligibility')
+            ->leftJoin('categories as cat', 'cat.id', '=', 'jobboard.category')
+            ->leftJoin('categories as scat', 'scat.id', '=', 'jobboard.subcategory')
+            ->leftJoin('qualification as ql', 'ql.id', '=', 'jobboard.qualification')
+            ->leftJoin('eligibility as el', 'el.id', '=', 'jobboard.eligibility')
             ->where('jobboard.id', '=', $id )
             ->select('jobboard.id','cat.category as category', 'scat.category as subcategory', 'ql.title as qualification', 'el.title as eligibility','logo','jobboard.job_name','jobboard.job_notification','jobboard.imp_date','jobboard.no_of_post','jobboard.application_fees')->get();
            dd(DB::getQueryLog());
